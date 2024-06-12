4309d3a3112151e6d2940dd0e3eacf41cbffd5e5
bokhtiar-boo <bokhtiar@boo.world>
2024-06-12 15:23:43 +0600

    added profile update

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
-const { getProfile, createProfile } = require('../handlers/profileHandler');
+const { getProfile, createProfile, updateProfile } = require('../handlers/profileHandler');
 
 module.exports = function () {
 	router.get('/:_id', getProfile);
 	router.post('/', createProfile);
+	router.patch('/:_id', updateProfile);
 	return router;
 };
